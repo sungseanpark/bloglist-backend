0ee21f5ed680017074b15fe94b15d31ee97b5acf
sungseanpark <sungseanpark@gmail.com>
2023-09-10 15:45:23 -0700

    Complete part 4.4

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,7 +1,18 @@
 const dummy = (blogs) => {
   return 1;
 }
+
+const totalLikes = (blogs) => {
+  if (blogs.length === 0) {
+    return 0;
+  }
+  const listOfLikes = blogs.map(blog => blog.likes)
+  const reducer = (sum, item) => {
+    return sum + item
+  }
+  return listOfLikes.reduce(reducer, 0)
+}
   
 module.exports = {
-  dummy
+  dummy, totalLikes
 }
