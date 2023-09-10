ade392809f840ec7738854f500ba2a86b956f72c
sungseanpark <sungseanpark@gmail.com>
2023-09-10 16:22:47 -0700

    Complete part 4.5

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -12,7 +12,29 @@ const totalLikes = (blogs) => {
   }
   return listOfLikes.reduce(reducer, 0)
 }
+
+const favoriteBlog = (blogs) => {
+  if (blogs.length === 0) {
+    return 0;
+  }
+  const formattedBlogs = blogs.map(function(blog) {
+    return {title: blog.title, author: blog.author, likes: blog.likes} 
+  })
+  console.log(formattedBlogs)
+
+  if(formattedBlogs.length === 1) {
+    return formattedBlogs[0]
+  }
+  
+  const reducer = (maxSoFar, current) => {
+    return maxSoFar.likes >= current.likes
+      ? maxSoFar
+      : current
+  }
+
+  return formattedBlogs.reduce(reducer)
+}
   
 module.exports = {
-  dummy, totalLikes
+  dummy, totalLikes, favoriteBlog
 }
