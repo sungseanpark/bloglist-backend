e83c5898df9c3193b44d677925d6a6db9651df37
sungseanpark <sungseanpark@gmail.com>
2024-01-14 20:27:26 -0800

    Complete part 4.13

--- tests/bloglist_api.test.js
+++ tests/bloglist_api.test.js
@@ -1,32 +1,15 @@
 const mongoose = require('mongoose')
 const supertest = require('supertest')
+const helper = require('./test_helper')
 const app = require('../app')
 const api = supertest(app)
 
 const Blog = require('../models/blog')
 
-const initialBlogs = [
-    {
-      title: "React patterns",
-      author: "Michael Chan",
-      url: "https://reactpatterns.com/",
-      likes: 7,
-    },
-    {
-      title: "Go To Statement Considered Harmful",
-      author: "Edsger W. Dijkstra",
-      url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
-      likes: 5,
-    },
-]
-
- beforeEach(async () => {
+beforeEach(async () => {
     await Blog.deleteMany({})
-    let blogObject = new Blog(initialBlogs[0])
-    await blogObject.save()
-    blogObject = new Blog(initialBlogs[1])
-    await blogObject.save()
- })
+    await Blog.insertMany(helper.initialBlogs)
+})
 
 
 test('blog posts are returned as json', async () => {
@@ -39,7 +22,7 @@ test('blog posts are returned as json', async () => {
 test('correct amount of blog posts are returned', async () => {
     const response = await api.get('/api/blogs')
 
-    expect(response.body).toHaveLength(initialBlogs.length)
+    expect(response.body).toHaveLength(helper.initialBlogs.length)
 })
 
 test('unique identifier property of blog posts is named id', async() => {
@@ -62,8 +45,29 @@ test('a valid blog post can be added', async() => {
       .expect('Content-Type', /application\/json/)
 
     const blogs = await Blog.find({})
-    expect(blogs).toHaveLength(initialBlogs.length + 1)
+    expect(blogs).toHaveLength(helper.initialBlogs.length + 1)
 })
+
+describe('deletion of a blog post', () => {
+    test('succeeds with status code 204 if id is valid', async () => {
+      const blogsAtStart = await helper.blogsInDb()
+      const blogToDelete = blogsAtStart[0]
+  
+      await api
+        .delete(`/api/blogs/${blogToDelete.id}`)
+        .expect(204)
+  
+      const blogsAtEnd = await helper.blogsInDb()
+  
+      expect(blogsAtEnd).toHaveLength(
+        helper.initialBlogs.length - 1
+      )
+  
+      const titles = blogsAtEnd.map(r => r.title)
+  
+      expect(titles).not.toContain(blogToDelete.title)
+    })
+  })
 
 afterAll(async () => {
     await mongoose.connection.close()
